5cf71f8dc798e653b06feebe9c92aa11a697aee1
lilhamster <60378510+lilhamster@users.noreply.github.com>
2021-02-21 23:58:23 -0500

    Update Remove Duplicates from Sorted Array.java

--- Remove Duplicates from Sorted Array.java
+++ Remove Duplicates from Sorted Array.java
@@ -1,12 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int n = 0;
-	    for (int i = 0; i < nums.length; i++) {
-		    if (nums[n] != nums[i]) {
-			    nums[++n] = nums[i];
-		    }
-	    }
-	    return ++n;
+	for (int i = 0; i < nums.length; i++) {
+		if (nums[n] != nums[i]) {
+			nums[++n] = nums[i];
+		}
+	}
+	return ++n;
         
     }
 }
+
+//Two pointer
+//Keep moving the ith pointer, when n find something diff set it to the position
+
